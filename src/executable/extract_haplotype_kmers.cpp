a59055bda28f347127fa0b4b27cc3b9aec6fbe8b
Ryan <rlorigro@ucsc.edu>
2021-08-30 17:54:38 -0700

    Include terminal kmers in paths now

--- src/executable/extract_haplotype_kmers.cpp
+++ src/executable/extract_haplotype_kmers.cpp
@@ -232,15 +232,96 @@ bool HaplotypePathKmer::update_has_diploid(){
 }
 
 
+pair<handle_t, bool> find_singleton_adjacent_handle(const PathHandleGraph& graph, const handle_t& h, bool left){
+    handle_t adjacent_handle;
+    size_t n_adjacent = 0;
+    bool success = false;
+
+    graph.follow_edges(h, left, [&](const handle_t& other_handle){
+        if (n_adjacent > 0){
+            return false;
+        }
+
+        adjacent_handle = other_handle;
+        n_adjacent++;
+
+        return true;
+    });
+
+    // Only return true if there was exactly one adjacent handle
+    if (n_adjacent == 1){
+        success = true;
+    }
+
+    return {adjacent_handle, success};
+}
+
+
+void extend_paths(MutablePathMutableHandleGraph& graph){
+    vector<pair <path_handle_t, handle_t> > to_be_prepended;
+    vector<pair <path_handle_t, handle_t> > to_be_appended;
+
+    graph.for_each_path_handle([&](const path_handle_t& p){
+        auto begin_handle = graph.get_handle_of_step(graph.path_begin(p));
+        auto end_handle = graph.get_handle_of_step(graph.path_back(p));
+
+        auto left_result = find_singleton_adjacent_handle(graph, begin_handle, true);
+        auto right_result = find_singleton_adjacent_handle(graph, end_handle, false);
+
+        if (left_result.second){
+            size_t n_paths = 0;
+            graph.for_each_step_on_handle(left_result.first, [&](const step_handle_t& s){
+                n_paths++;
+            });
+
+            if (n_paths == 0) {
+                to_be_prepended.emplace_back(p, left_result.first);
+            }
+            else{
+                throw runtime_error("ERROR: node left of haplotype path " + graph.get_path_name(p) + " is covered by other paths");
+            }
+        }
+
+        if (right_result.second){
+            size_t n_paths = 0;
+            graph.for_each_step_on_handle(right_result.first, [&](const step_handle_t& s){
+                n_paths++;
+            });
+
+            if (n_paths == 0) {
+                to_be_appended.emplace_back(p, right_result.first);
+            }
+            else{
+                throw runtime_error("ERROR: node right haplotype path " + graph.get_path_name(p) + " is covered by other paths");
+            }
+        }
+    });
+
+    for (auto& item: to_be_prepended){
+        // PREpend the LEFT side node if it meets the conditions
+        graph.prepend_step(item.first, item.second);
+    }
+
+    for (auto& item: to_be_appended){
+        // Append the RIGHT side node if it meets the conditions
+        graph.append_step(item.first, item.second);
+    }
+}
+
+
 void extract_haplotype_kmers_from_gfa(path gfa_path, size_t k){
     HashGraph graph;
     IncrementalIdMap<string> id_map;
 
     gfa_to_handle_graph(graph, id_map, gfa_path);
 
-    cerr << "Iterating paths" << '\n';
-
     plot_graph(graph, "start_graph");
+
+    cerr << "Extending paths into haploid regions..." << '\n';
+
+    extend_paths(graph);
+
+    cerr << "Iterating path kmers..." << '\n';
 
     bool prev_has_diploid;
 
