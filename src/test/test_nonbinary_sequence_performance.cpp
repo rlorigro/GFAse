6752d92fb0f24f4e0837c555df0fd48102fa9d1e
Ryan <rlorigro@ucsc.edu>
2021-09-07 14:18:04 -0700

    Add tests for making performance easier to evaluate

---
+++ src/test/test_nonbinary_sequence_performance.cpp
@@ -0,0 +1,90 @@
+#include "HaplotypePathKmer.hpp"
+#include "IncrementalIdMap.hpp"
+#include "BinarySequence.hpp"
+#include "gfa_to_handle.hpp"
+#include "handle_to_gfa.hpp"
+#include "GraphUtility.hpp"
+#include "Filesystem.hpp"
+#include "GfaReader.hpp"
+#include "CLI11.hpp"
+
+#include "unordered_set"
+#include "map"
+#include "bdsg/hash_graph.hpp"
+
+#include <string>
+
+using gfase::HaplotypePathKmer;
+using gfase::IncrementalIdMap;
+using gfase::BinarySequence;
+using gfase::for_each_connected_component;
+using gfase::split_connected_components;
+using gfase::handle_graph_to_gfa;
+using gfase::print_graph_paths;
+using gfase::plot_graph;
+
+using ghc::filesystem::path;
+using bdsg::HashGraph;
+using bdsg::MutablePathMutableHandleGraph;
+using bdsg::MutablePathDeletableHandleGraph;
+using handlegraph::path_handle_t;
+using handlegraph::step_handle_t;
+using handlegraph::handle_t;
+
+using std::string;
+using std::cout;
+using std::cerr;
+using std::unordered_set;
+using std::map;
+using std::runtime_error;
+
+
+void strip_trailing_space(string& line){
+    size_t n_trailing_spaces = 0;
+    for (auto iter = line.rbegin(); iter != line.rend(); iter++){
+        if (isspace(*iter)){
+            n_trailing_spaces++;
+        }
+    }
+
+    line.resize(line.size() - n_trailing_spaces);
+}
+
+
+void test_performance(path kmer_file_path){
+    ifstream file(kmer_file_path);
+    string line;
+
+    unordered_set <string> kmers;
+
+    while (getline(file, line)){
+        if (line[0] == '>'){
+            continue;
+        }
+        else{
+            strip_trailing_space(line);
+            kmers.emplace(line);
+        }
+    }
+}
+
+
+int main (int argc, char* argv[]){
+    path kmer_file_path;
+
+    CLI::App app{"App description"};
+
+    app.add_option(
+            "-i,--input",
+            kmer_file_path,
+            "Path to FASTA containing kmers to load into memory")
+            ->required();
+
+    CLI11_PARSE(app, argc, argv);
+
+    test_performance(kmer_file_path);
+
+    return 0;
+}
+
+
