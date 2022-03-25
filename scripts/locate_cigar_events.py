136a99876420012ba32c30e4d0e9c5c7bb35b296
Ryan <rlorigro@ucsc.edu>
2022-03-25 11:01:37 -0700

    Add hashbang and output file

--- scripts/locate_cigar_events.py
+++ scripts/locate_cigar_events.py
@@ -1,28 +1,32 @@
+#!/usr/bin/env python3
 from modules.Paf import *
 import argparse
 
 
-def locate_cigar_events(paf_path, indel_threshold):
+def locate_cigar_events(paf_path, output_path, indel_threshold):
 
     n_inserts = 0
     n_deletes = 0
 
     reversal_char = ['+','-']
 
-    print(','.join(["line", "ref_name", "ref_coord", "query_name", "query_coord", "reversal", "operation", "length", "map_quality"]))
+    with open(paf_path, 'r') as file, open(output_path, 'w') as output_file:
+        output_file.write(','.join(["line", "ref_name", "ref_coord", "query_name", "query_coord", "reversal", "operation", "length", "map_quality"]))
+        output_file.write('\n')
 
-    with open(paf_path, 'r') as file:
         for l,line in enumerate(file):
             element = PafElement(line, store_tags=True)
 
             for ref_coord,query_coord,operation,length in iterate_cigar(element):
-                if length > indel_threshold:
+                if length >= indel_threshold:
                     if operation == 'I':
                         n_inserts += 1
-                        print(','.join(list(map(str,[l,element.get_ref_name(), ref_coord, element.get_query_name(), query_coord, reversal_char[element.get_reversal()], operation, length, element.get_map_quality()]))))
+                        output_file.write(','.join(list(map(str,[l,element.get_ref_name(), ref_coord, element.get_query_name(), query_coord, reversal_char[element.get_reversal()], operation, length, element.get_map_quality()]))))
+                        output_file.write('\n')
                     if operation == 'D':
                         n_deletes += 1
-                        print(','.join(list(map(str,[l,element.get_ref_name(), ref_coord, element.get_query_name(), query_coord, reversal_char[element.get_reversal()], operation, length, element.get_map_quality()]))))
+                        output_file.write(','.join(list(map(str,[l,element.get_ref_name(), ref_coord, element.get_query_name(), query_coord, reversal_char[element.get_reversal()], operation, length, element.get_map_quality()]))))
+                        output_file.write('\n')
 
     # print("n_inserts", n_inserts)
     # print("n_deletes", n_deletes)
@@ -39,6 +43,13 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
+        "-o",
+        required=True,
+        type=str,
+        help="Output file, where cigar events will be written"
+    )
+
+    parser.add_argument(
         "-l",
         required=True,
         type=int,
@@ -47,5 +58,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    locate_cigar_events(paf_path=args.i, indel_threshold=args.l)
-
+    locate_cigar_events(paf_path=args.i, output_path=args.o, indel_threshold=args.l)
