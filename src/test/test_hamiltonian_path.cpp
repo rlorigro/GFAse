c2216f41c2a8c12384098e0d03bf04a67c26a7cc
Jordan Eizenga <joeizeng@gmail.com>
2023-01-20 16:50:28 -0800

    add hamiltonian path to build

---
+++ src/test/test_hamiltonian_path.cpp
@@ -0,0 +1,30 @@
+#include "HamiltonianPath.hpp"
+#include "IncrementalIdMap.hpp"
+#include "gfa_to_handle.hpp"
+#include "handle_to_gfa.hpp"
+#include "graph_utility.hpp"
+#include "Filesystem.hpp"
+
+#include "bdsg/hash_graph.hpp"
+
+#include <string>
+#include <set>
+#include <iostream>
+#include <algorithm>
+
+using namespace gfase;
+
+using ghc::filesystem::path;
+using bdsg::HashGraph;
+using handlegraph::handle_t;
+
+using std::sort;
+using std::string;
+using std::cerr;
+using std::endl;
+
+int main(){
+
+    
+    return 0;
+}
